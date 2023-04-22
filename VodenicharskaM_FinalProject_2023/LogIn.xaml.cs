62b522b086a43a9d918fdee5961fa0b7aa9789e9
Bes-tie <m.vodenicharska23@acsbg.org>
2023-04-22 16:55:42 +0300

    dimesions fixed login signup finished jewelry started

--- VodenicharskaM_FinalProject_2023/LogIn.xaml.cs
+++ VodenicharskaM_FinalProject_2023/LogIn.xaml.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace VodenicharskaM_FinalProject_2023
@@ -19,9 +20,41 @@ namespace VodenicharskaM_FinalProject_2023
     {
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            BrJewl logCont = new BrJewl();
-            logCont.Show();
-            this.Close();
+                SqlConnection con = new SqlConnection(@"Data Source = DESKTOP-SDEJ6HG; Initial Catalog=Mivoski; Integrated Security=True");
+
+            try
+            {
+                con.Open();
+                string query = "SELECT * FROM Customers Where Username=@Username and Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, con);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUser.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", pass.Password);
+
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    MessageBox.Show("Welcome, {0}!", txtUser.Text);
+                    BrJewl logCont = new BrJewl();
+                    logCont.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password are not correct!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
--- VodenicharskaM_FinalProject_2023/SignUp.xaml.cs
+++ VodenicharskaM_FinalProject_2023/SignUp.xaml.cs
@@ -22,11 +22,11 @@ namespace VodenicharskaM_FinalProject_2023
     {
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\m.vodenicharska23\Source\Repos\VodenicharskaM_FinalProject_2023\VodenicharskaM_FinalProject_2023\MivoskiDB.mdf; Integrated Security = True");
+            SqlConnection con = new SqlConnection(@"Data Source = DESKTOP-SDEJ6HG; Initial Catalog=Mivoski; Integrated Security=True");
             try
             {
                 con.Open();
-                string query = "INSERT INTO Customers_SU(name, surname, username, email, password)values('" + this.txtName.Text + "','" + this.txtSurname.Text + "','" + this.txtUser.Text + "','" + this.txtEmail.Text + "','" + this.pass.Password + "')";
+                string query = "INSERT INTO Customers(name, surname, username, email, password)values('" + this.txtName.Text + "','" + this.txtSurname.Text + "','" + this.txtUser.Text + "','" + this.txtEmail.Text + "','" + this.pass.Password + "')";
                 SqlCommand cmd = new SqlCommand(query, con);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Your account has been saved!");
